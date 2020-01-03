e6d092d69f26f3363ecb012d5d122f2e6b539184
Kug3n <50046541+Kug3n@users.noreply.github.com>
2020-01-03 14:12:36 +0400

    First commit

---
+++ js/custom.js
@@ -0,0 +1,60 @@
+//
+/* ---------------START--------------integration point metro PULSATION ECHEC-------------------------------- */
+
+// var cssIcon1 = L.divIcon({
+//   className: 'css-icon1',
+//   html: '<div class="transportation-station-1"></div>'
+// });
+// var cssIcon2 = L.divIcon({
+// className: 'css-icon2',
+// html: '<div class="transportation-station-2"></div>'
+// });
+// var cssIcon3 = L.divIcon({
+// className: 'css-icon3',
+// html: '<div class="transportation-station-3"></div>'
+// });
+// var cssIcon4 = L.divIcon({
+// className: 'css-icon4',
+// html: '<div class="transportation-station-4"></div>'
+// });
+// var cssIcon5 = L.divIcon({
+// className: 'css-icon5',
+// html: '<div class="transportation-station-5"></div>'
+// });
+// var cssIcon6 = L.divIcon({
+// className: 'css-icon6',
+// html: '<div class="transportation-station-6"></div>'
+// });
+// var cssIcon7 = L.divIcon({
+// className: 'css-icon7',
+// html: '<div class="transportation-station-7"></div>'
+// });
+// var cssIcon8 = L.divIcon({
+// className: 'css-icon8',
+// html: '<div class="transportation-station-8"></div>'
+// });
+// var cssIcon9 = L.divIcon({
+// className: 'css-icon9',
+// html: '<div class="transportation-station-9"></div>'
+// });
+//
+// var cssIcon10 = L.divIcon({
+// className: 'css-icon10',
+// html: '<div class="transportation-station-10"></div>'
+// });
+//
+//
+//
+//
+
+/* ---------------START---------------test bihanic------------------------------- */
+
+// var cssIcon = L.divIcon({
+// className: 'css-icon',
+// html: '<div class="gps_ring"></div>'
+// });
+//
+// var cssIcon2 = L.divIcon({
+// className: 'css-icon2',
+// html: '<div class="gps_ring2"></div>'
+// });
